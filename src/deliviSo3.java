a76d5a023a4a80d915606ee1348f99d7531cd28c
unknown <aleksandarristov28@gmail.com>
2023-12-23 12:46:19 +0100

    first java program

---
+++ src/deliviSo3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class deliviSo3 {
+	
+	public static void main(String[] args) {
+				
+				Scanner input = new Scanner(System.in);
+				
+				System.out.println("Da se ispecatat clenovite na niza koi ne se delivi so 3");
+				System.out.println("Vnesi broj na clenovi vo nizata ");
+				int n = input.nextInt();
+				
+				int niza[] = new int[n];
+		        for (int i =0; i < niza.length; i++) 
+		        {
+		        	System.out.print(i +"-->");	
+		        	niza[i] = input.nextInt();//vcitaj niza[i] - i-ti clen vo niza
+				}
+				for(int i=0; i < niza.length; i++) {
+					if(niza[i] % 3!=0) {
+						System.out.println("niza[" + i + "]=" + niza[i]);	
+					}
+						
+					
+				}
+			}
+		}
+
+
